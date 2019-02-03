229a0162f4ed3cb8310c032f45786b18aa76b304
Sunny Jhun Ibabao <sunnyjhun.ibabao@gmail.com>
2019-02-03 13:19:39 +0800

    includes change in view.php

--- app/views/home.php
+++ app/views/home.php
@@ -3,15 +3,49 @@
 
 <?php function get_page_content(){ ?>
 
-<section>
-	<div class="container-fluid mb-5">
+	<section>
+	<!-- <div class="container-fluid mb-5">
 		<div class="jumbotron bg-dark" id="jumbo">
 			<div class="d-flex justify-content-center" id="inner-line">
 				<p class="text-center" id="brand">WeeGo</p>
 				<h1 class="text-center" >We make it go for you</h1>
 			</div>
 		</div>
+	</div> -->
+	<!-- Slideshow container -->
+	<div class="slideshow-container">
+
+		<!-- Full-width images with number and caption text -->
+		<div class="mySlides fade">
+			<div class="numbertext"></div>
+			<img  class="slide-img" src="../assets/images/fashion.jpeg" style="width:100%">
+			<div class="text">We make it go for you</div>
+		</div>
+
+		<div class="mySlides fade">
+			<div class="numbertext"></div>
+			<img class="slide-img" src="../assets/images/cars.jpeg" style="width:100%">
+			<div class="text">Buy the car you love</div>
+		</div>
+
+		<div class="mySlides fade">
+			<div class="numbertext"></div>
+			<img class="slide-img" src="../assets/images/house.jpeg" style="width:100%">
+			<div class="text">Have your comfortable dwelling</div>
+		</div>
+
+		<!-- Next and previous buttons -->
+		<!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+		<a class="next" onclick="plusSlides(1)">&#10095;</a> -->
 	</div>
+	<!-- <br> -->
+
+	<!-- The dots/circles -->
+	<!-- <div style="text-align:center">
+		<span class="dot" onclick="currentSlide(1)"></span> 
+		<span class="dot" onclick="currentSlide(2)"></span> 
+		<span class="dot" onclick="currentSlide(3)"></span> 
+	</div>  -->
 </section>
 
 
