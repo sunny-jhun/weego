cafe0a8bd5e38bb20076a178293b50bad310fa9c
Sunny Jhun Ibabao <sunnyjhun.ibabao@gmail.com>
2019-02-03 13:31:38 +0800

    includes revision

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -304,5 +304,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
